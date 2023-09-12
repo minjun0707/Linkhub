3653f46df98e3330eb6fc6b730efbe143c72c87f
minjun0707 <jmk7117@naver.com>
2023-09-13 07:48:12 +0900

    배포 테스트

--- src/main/java/com/example/goodWriting/common/WebMvcConfig.java
+++ src/main/java/com/example/goodWriting/common/WebMvcConfig.java
@@ -2,12 +2,14 @@ package com.example.goodWriting.common;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.example.goodWriting.domain.user.LoginInterceptor;
 
 @Configuration
+@EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer {
 
 
@@ -18,12 +20,14 @@ public class WebMvcConfig implements WebMvcConfigurer {
 			.addPathPatterns("/api/board/create/temp");
 	}
 
+
+
 	//쿠키 허용 allowCredentials
 	//allowCredentials 사용시  allowedOrigins 사용불가 따라서 allowedOriginPatterns 사용
 	public void addCorsMappings(final CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedOriginPatterns("*")
-			.allowedMethods("GET", "POST")
+			.allowedOriginPatterns("http://localhost:3000","http://localhost:3003","http://localhost:3002","http://linkhub.life","http://www.linkhub.life")
+			.allowedMethods("*")
 			.allowCredentials(true)
 			.maxAge(3000);
 	}
